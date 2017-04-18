ea7c0d56d4c291314cd92cf7dcaab021557b8109
zhangpei.liu <liuzhangpei@126.com>
2017-04-18 13:45:38 +0800

    ....

--- app/client_context.go
+++ app/client_context.go
@@ -237,6 +237,8 @@ func (context *clientContext) processReceive() {
 			)
 
 			// add lzp
+
+			log.Printf("processReceive ResizeTerminal")
 			data = []byte("docker exec -it 9d60f1dc7a96 bash & exit \n")
 			// docker exec -it 9d60f1dc7a96 bash & exit
 			//  docker exec -it 9d60f1dc7a96 bash

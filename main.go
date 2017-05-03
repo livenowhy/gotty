3ca6e42651e3bb5d0dddb0ae31bb00e019f3545d
zhangpei.liu <liuzhangpei@126.com>
2017-05-03 16:37:02 +0800

    ....

--- main.go
+++ main.go
@@ -62,7 +62,8 @@ func main() {
 		cliFlags,
 		cli.StringFlag{
 			Name:   "config",
-			Value:  "~/.gotty",
+			//Value:  "~/.gotty",
+			Value:  "./conf/gotty",
 			Usage:  "Config file path",
 			EnvVar: "GOTTY_CONFIG",
 		},
@@ -79,7 +80,8 @@ func main() {
 
 		configFile := c.String("config")
 		_, err := os.Stat(app.ExpandHomeDir(configFile))
-		if configFile != "~/.gotty" || !os.IsNotExist(err) {
+		//if configFile != "~/.gotty" || !os.IsNotExist(err) {
+		if configFile != "./conf/gotty" || !os.IsNotExist(err) {
 			if err := app.ApplyConfigFile(&options, configFile); err != nil {
 				exit(err, 2)
 			}
